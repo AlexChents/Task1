9a815d287078a8919e0218f0429b23196d26e874
krukovits <krukovits@ukr.net>
2024-02-11 22:29:12 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Task1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.InputEncoding = Encoding.Unicode;
+			Console.OutputEncoding = Encoding.Unicode;
+
+			string input = "The \"C# Professional\" course          includes the  topics I discuss in my CLR via C# book and teaches how the CLR works thereby showing you how to develop applications and reusable components for the .NET Framework.";
+			//char[] punctuationMarks = { '.', ',', '!', '?', ':',';' };
+			//input = input.Trim().Trim(punctuationMarks);
+			while (input.IndexOf("  ") != -1)
+			{
+				input = input.Replace("  ", " ");
+			}
+			string[] inputArray = input.Split(' ');
+			var groupWords = inputArray.Distinct().OrderBy(gw => gw.Length).GroupBy(gw => gw.Length);
+			foreach (var group in groupWords)
+			{
+				Console.WriteLine("Words of length: " + group.Key + ", Count: " + group.Count());
+				foreach (var word in group)
+				{
+					Console.WriteLine(word.ToString());
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
